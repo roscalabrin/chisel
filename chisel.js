b9938f604689d548d28a73d7ec7dc7cc5c7cca92
Roberta Doyle <ro.scalabrin@gmail.com>
2016-09-28 17:19:47 -0600

    level 2

--- chisel.js
+++ chisel.js
@@ -11,9 +11,39 @@ function converter(text) {
       eachline[i] = "<h1>" + eachline[i].slice(1, this.length) + "</h1>"; 
     } else {
       eachline[i] = "<p>" + eachline[i].slice(0, this.length) + "</p>"; 
+      eachline[i] = formatting(eachline[i]);
     }
     return eachline.join("\n");
 }
-console.log(converter("####Denver\nOther cities.\n#Header1\n###Header3"));
-console.log(converter("# Header\nfollowed by text"));
 
+function formatting(text) {
+  if (text.includes("**")) {
+    text = text.split("**")
+    for (var i = 0; i < text.length - 1; i++)
+      if ( i % 2 === 0) {
+        text[i] = text[i] + "<strong>";
+      } else {
+        text[i] = text[i] + "</strong>";
+      }
+    return formatting2(text.join(""));
+   // return text.join("");
+  }
+}
+
+function formatting2(text) {
+  if (text.includes("*")) {
+    text = text.split("*")
+    for (var i = 0; i < text.length -1; i++)
+      if ( i % 2 === 0) {
+        text[i] = text[i] + "<em>";
+      } else {
+        text[i] = text[i] + "</em>";
+      }
+    return text.join("");
+  } else {
+    return text;
+  }
+}
+
+console.log(converter("Denver is one of the **most** desirable *cities* to live.\n#Header1\n###Header3"));
+console.log(converter("My *emphasized and **stronged** text* is awesome."));
